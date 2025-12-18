cd2360c877bfcecb28551ca3e94afe35fe7654ea
Alisher Iskakov <pakapaka1995@mail.ru>
2025-12-18 05:24:10 +0100

    Added parallel routes

---
+++ app/archive/@archive/page.tsx
@@ -0,0 +1,7 @@
+export default function ArchivePage() {
+  return (
+    <div>
+      <h1>ArchivePage</h1>
+    </div>
+  );
+}
---
+++ app/archive/@latest/page.tsx
@@ -0,0 +1,7 @@
+export default function LatestNewsPage() {
+  return (
+    <div>
+      <h1>LatestNewsPage</h1>
+    </div>
+  );
+}
