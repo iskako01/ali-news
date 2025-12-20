d9348808ea70df578e3d1af25f5cb6e8355e7bc9
Alisher Iskakov <pakapaka1995@mail.ru>
2025-12-20 13:20:34 +0100

    Building APIs with Route Handlers

---
+++ app/api/route.ts
@@ -0,0 +1,8 @@
+export function GET(request) {
+  console.log(request);
+
+  return new Response("Hello, Next.js API Route!");
+}
+// export function POST(request) {
+//   console.log(request);
+// }
