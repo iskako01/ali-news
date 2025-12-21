9305b878d134c155395a71d9774779c14d3010ff
Alisher Iskakov <pakapaka1995@mail.ru>
2025-12-21 11:39:10 +0100

    Fetching news data from BE

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,12 +1,51 @@
+"use client";
+
 import NewsList from "@/components/news/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const newsData = await response.json();
+
+      setIsLoading(false);
+      setNews(newsData);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return (
+      <>
+        <h1>News Page</h1>
+        <p>{error}</p>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>News Page</h1>
 
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
