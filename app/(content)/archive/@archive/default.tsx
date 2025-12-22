a7584260bb9e9227e2b57d5c76ecf822c526e787
Alisher Iskakov <pakapaka1995@mail.ru>
2025-12-22 06:31:54 +0100

    Refactoring parallel route

---
+++ app/(content)/archive/@archive/default.tsx
@@ -0,0 +1,10 @@
+import NewsList from "@/components/news/news-list";
+import { getLatestNews } from "@/lib/news";
+
+export default function ArchiveNewsPage() {
+  return (
+    <div>
+      <h1>ArchiveNews Default</h1>
+    </div>
+  );
+}
--- app/(content)/layout.jsx
+++ app/(content)/layout.jsx
@@ -1,19 +1,15 @@
-import MainHeader from '@/components/main-header/main-header';
- 
+import MainHeader from "@/components/main-header/main-header";
+
 export const metadata = {
-  title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
+  title: "Next.js Page Routing & Rendering",
+  description: "Learn how to route to different pages.",
 };
- 
+
 export default function ContentLayout({ children }) {
   return (
-  <html lang="en">
-      <body>
-        <div className="page">
-          <MainHeader />
-          {children}
-        </div>
-      </body>
-    </html>
+    <div className="page">
+      <MainHeader />
+      {children}
+    </div>
   );
 }
